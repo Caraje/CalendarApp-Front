6eb312969d0df9097e6d88838713eef53eb87610
Carlos Ajenjo <Carlosajenjo@gmail.com>
2022-04-05 17:57:16 +0200

    primer commit

---
+++ src/types/types.js
@@ -0,0 +1,25 @@
+
+export const types = {
+
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+
+    eventSetActive: '[event] Set Active',
+    eventStartAddNew: '[EVENT] Start add New',
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+    eventLoaded: '[EVENT] Evento loaded',
+    eventLogout: '[EVENT] Event Logout',
+
+
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start Register',
+    authStartStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout',
+
+}
